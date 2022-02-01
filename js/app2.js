39a558e73f7e04609990eca081ca70a9a207b268
Oswaldo Mendez <oswaluis8@gmail.com>
2022-02-01 11:13:33 -0300

    primera pre-entrega

--- js/app2.js
+++ js/app2.js
@@ -1,13 +1,10 @@
 let nombre = prompt("¿Como te llamas?")
 alert("Bievenido " + nombre + " tenemos productos nuevos para ti");
+console.log("Cliente = " + nombre)
 
-let mostrarProductos = ["remera colores","remera seleccion","remera negra","remera flores","pantalon negro","pantalon azul", "pantalon verde",
- 						"short negro", "short verde", "short seleccion", "medias cortas", "medias largas"]
-
-alert(mostrarProductos.join ("\n"))
 let Productos =[
 	{id:1 , nombre:"remera colores" , tipo: "remera" , precio: 1100},
-	{id:2, nombre:"remera seleccion", tipo: "remera" , desc: 25 , precio: 1100},
+	{id:2, nombre:"remera seleccion", tipo: "remera" , precio: 1100},
 	{id:3, nombre:"remera negra", tipo: "remera" ,  precio: 1100},
 	{id:4, nombre:"remera flores", tipo: "remera" ,  precio: 1100},
 	{id:5, nombre:"pantalon negro", tipo: "pantalon" ,  precio: 2000},
@@ -19,6 +16,9 @@ let Productos =[
 	{id:11, nombre:"medias largas", tipo: "medias" ,  precio: 500},
 	{id:12, nombre:"medias cortas", tipo: "medias" ,  precio: 500},
 ]
+let mostrarProductos = Productos.map((pro)=> pro.nombre)
+alert(mostrarProductos.join ("\n"))
+
 
 let carrito = []
 
@@ -26,30 +26,36 @@ agregar()
 
 function agregar() {
 	let  userQuiere = prompt ("¿Cual producto quiere?").toLowerCase()
+	let cantidadUser = parseInt(prompt("¿cuantos"))
 
 	let agregar = Productos.find((cual) => cual.nombre == userQuiere)
+
+	
+	agregar.cantidad = cantidadUser
 	carrito.push(agregar)
 	console.log(carrito)
 carritoNew()
 	
 }
+
+
+
+
 otro()
 function otro()
-{let otroProducto = prompt ("¿Desea agregar otro producto").toLowerCase()
+{let otroProducto = prompt (nombre +" ¿Desea agregar otro producto").toLowerCase()
 		if (otroProducto == "si") {
 			agregar()
 			otro()
 		} else {
-		alert("muchas gracias")
+		alert("muchas gracias " + nombre)
 	}
-
 }
 	
 
-
 function carritoNew(){
 	console.log("cantidad de productos agregados " + carrito.length)
-	 suma = carrito.reduce((acc, cual)=> acc + cual.precio , 0)
+	 suma = carrito.reduce((acc, cual)=> acc + (cual.precio * cual.cantidad), 0)
 
 	console.log("El total de su carrito es de $" + suma)
 	return suma
@@ -57,7 +63,7 @@ function carritoNew(){
 
 console.log (suma)
 let total = suma
-console.log (total)
+
 let descuento = prompt("Eres miembro de nuestra tienda?").toLowerCase();
 
 function descuentoCal(){
